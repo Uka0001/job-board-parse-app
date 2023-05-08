3da7f39b050d553a069a1cc130615c939d95dfee
Uka0001 <ukraine0001@gmail.com>
2023-05-08 20:46:10 +0300

    task completed, tests need to be done

--- src/main/java/com/example/arbeitnowapiapp/ArbeitnowApiAppApplication.java
+++ src/main/java/com/example/arbeitnowapiapp/ArbeitnowApiAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class ArbeitnowApiAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ArbeitnowApiAppApplication.class, args);
     }
--- src/main/java/com/example/arbeitnowapiapp/repository/JobRepository.java
+++ src/main/java/com/example/arbeitnowapiapp/repository/JobRepository.java
@@ -1,14 +1,23 @@
 package com.example.arbeitnowapiapp.repository;
 
+import com.example.arbeitnowapiapp.dto.CityDto;
 import com.example.arbeitnowapiapp.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
-    @Query(value = "SELECT DISTINCT * FROM job_data j WHERE slug = ?1 AND CREATED_AT = ?2", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT *"
+            + " FROM job_data j"
+            + " WHERE slug = ?1 AND CREATED_AT = ?2",
+            nativeQuery = true)
     List<Job> findAllBySlugsAndCreationTime(String slug, long createdAt);
 
-
+    @Query("""
+            SELECT new com.example.arbeitnowapiapp.dto.CityDto(j.location, count(j))
+            FROM Job j
+            GROUP BY j.location
+            ORDER BY count(j) ASC
+            """)
+    List<CityDto> getStatisticByCity();
 }
